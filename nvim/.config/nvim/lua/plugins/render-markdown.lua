fec20047c52163ff5ac55d7b016fb1934a184a70
Daniel Souza <danielt3108@gmail.com>
2026-07-16 21:42:20 -0300

    feat: added render-markdown plugin

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+---@diagnostic disable-next-line: undefined-doc-name
+    ---@type render.md.UserConfig
+    opts = {},
+}
